0ee1b3c63b49de1dc9d3fa36f7b5d26081c0eeb4
Kian Alex Mesforush <74278881+kmesfor@users.noreply.github.com>
2025-07-27 17:03:33 -0400

    Create new executable for GenerateGraphFiles

---
+++ src/backend/graph-generator/GenerateGraphFiles.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Kian Mesforush on 7/27/25.
+//
+#include <iostream>
+
+/**
+ * This is a part of a new executable named GenerateGraphFiles used to generate SkylinkGraph json files from
+ * raw data (stored in dist/data/generated). Ideally, these scripts will only need to be run during development,
+ * once a file is finalized for running the main program, they will not need to be changed.
+ */
+int main() {
+	std::cout << "Hello world" << std::endl;
+}
