b6bd9dc7a815c0e1a6dba2ffb01c2347f2d855fe
Kian Alex Mesforush <74278881+kmesfor@users.noreply.github.com>
2025-07-27 16:24:33 -0400

    Add from_json method to SkylinkGraph, add comments, update memory management setup

--- src/backend/datamodels/SkylinkGraph.h
+++ src/backend/datamodels/SkylinkGraph.h
@@ -10,31 +10,76 @@
 #include <vector>
 #include "../resources/json.hpp"
 
-
 #include "Airport.h"
 #include "AirportRoute.h"
 
 using AirportCode = std::string;
 using json = nlohmann::json;
 
+/**
+ * Class to represent a collection of Airports and AirportRoutes in a graph.
+ * Lifecycle should be managed by DataManager
+ */
 struct SkylinkGraph {
-	/* This operates like an adjacency list, but formatted differently.
+	// Ensures that only DataManager can instantiate a SkylinkGraph, ensuring that SkylinkGraph's lifecycle is managed by DataManager
+	friend class DataManager;
+private:
+
+	/**
+	 * Constructor. Lifecycle should be managed by DataManager. Prevents instantiation from a public scope
+	 */
+	SkylinkGraph() = default;
+
+	/**
+	 * Destructor. Lifecycle should be managed by DataManager. Prevents destruction from a public scope
+	 */
+	~SkylinkGraph() {
+		for (const auto& airport : airport_lookup) {
+			delete airport.second;
+		}
+	}
+
+	/**
+	 * Create a SkylinkGraph object from JSON. Lifecycle should be managed by DataManager
+	 * @param data nlohman::json data
+	 * @return SkylinkGraph object pointer
+	 */
+	static SkylinkGraph* from_json(json data) {
+		// Initialize a new graph object (deleted from memory on save)
+		auto* graph = new SkylinkGraph();
+		graph->airports = data.at("airports");
+
+		for (const auto& airport_elem : data.at("airport_lookup")) {
+			auto* airport = Airport::from_json(airport_elem);
+			graph->airport_lookup[airport->code] = airport;
+		}
+		return graph;
+	}
+public:
+	/**
+	 * Operates like an adjacency list, but formatted differently.
 	 * A list of airport codes is stored, from that, airport objects can be retrieved
 	 * from their airport code. Each airport object stores its outgoing edges, creating
-	 * an adjacency list setup. Allows each airport to own its routes and have more direct access.
-	 * Also allows the frontend to easily have a list of all the airports without grabbing all their associated data
+	 * an adjacency list structure. Allows each airport to own its routes and have direct access
+	 * Also allows the frontend to easily have a list of all airports without loading all associated data
 	 */
 	std::vector<AirportCode> airports;
 
-	// Lookup map to convert an airport code to airport object
-	std::unordered_map<AirportCode, Airport> airport_lookup;
+	/**
+	 * Lookup map to get an Airport object from its AirportCode. i.e. "IAD"
+	 */
+	std::unordered_map<AirportCode, Airport*> airport_lookup;
 
+	/**
+	 * Convert the graph data into JSON format
+	 * @return nlohmann::json
+	 */
 	[[nodiscard]] std::string to_json() const {
 		json data;
 		data["airports"] = airports;
 
 		for (const auto& airport : airport_lookup) {
-			data["airport_lookup"][airport.first] = airport.second.to_json();
+			data["airport_lookup"][airport.first] = airport.second->to_json();
 		}
 
 		return data.dump();
