ed1c8966c8b034e9b63a8f1db4528abdf8a13fcf
Kian Alex Mesforush <74278881+kmesfor@users.noreply.github.com>
2025-07-28 11:49:14 -0400

    Make C++ version 17 for filesystem usage

--- src/backend/graph-generator/GenerateGraphFiles.cpp
+++ src/backend/graph-generator/GenerateGraphFiles.cpp
@@ -3,11 +3,18 @@
 //
 #include <iostream>
 
+#include "backend/DataManager.h"
+
+const std::string GRAPH_FILEPATH = "./data/generated/skylinkgraph.json";
+
 /**
  * This is a part of a new executable named GenerateGraphFiles used to generate SkylinkGraph json files from
  * raw data (stored in dist/data/generated). Ideally, these scripts will only need to be run during development,
  * once a file is finalized for running the main program, they will not need to be changed.
  */
 int main() {
-	std::cout << "Hello world" << std::endl;
+	DataManager manager(GRAPH_FILEPATH);
+
+	std::cout << manager.graph->to_json();
+	manager.save();
 }
